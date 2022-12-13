8b6b7b3d23a14c3a2272ab2cae97456603b6aaec
ProgrammerTabish <programmertabish@gmail.com>
2022-12-13 19:11:57 +0530

    updated db schema

--- back_end/database/project_5th_sem.sql
+++ back_end/database/project_5th_sem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 12, 2022 at 04:21 AM
+-- Generation Time: Dec 13, 2022 at 02:38 PM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -75,7 +75,7 @@ CREATE TABLE `documents` (
 
 CREATE TABLE `property` (
   `ulpin` varchar(14) NOT NULL,
-  `uid` int(12) DEFAULT NULL,
+  `uid` varchar(15) DEFAULT NULL,
   `registered_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp(),
   `approval_status` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -88,7 +88,7 @@ CREATE TABLE `property` (
 
 CREATE TABLE `reviews` (
   `ulpin` varchar(14) NOT NULL,
-  `uid` int(12) NOT NULL,
+  `uid` varchar(15) NOT NULL,
   `rating` smallint(2) NOT NULL,
   `comment` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -100,7 +100,7 @@ CREATE TABLE `reviews` (
 --
 
 CREATE TABLE `user` (
-  `uid` int(12) NOT NULL,
+  `uid` varchar(15) NOT NULL,
   `type` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -111,11 +111,12 @@ CREATE TABLE `user` (
 --
 
 CREATE TABLE `user_info` (
-  `uid` int(12) NOT NULL,
+  `uid` varchar(15) NOT NULL,
   `name` varchar(25) NOT NULL,
   `email` varchar(40) NOT NULL,
   `city` varchar(25) NOT NULL,
-  `state` varchar(25) NOT NULL
+  `state` varchar(25) NOT NULL,
+  `password` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -125,7 +126,7 @@ CREATE TABLE `user_info` (
 --
 
 CREATE TABLE `user_profile` (
-  `uid` int(12) NOT NULL,
+  `uid` varchar(15) NOT NULL,
   `profile_image` text NOT NULL,
   `about` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
